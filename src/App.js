2f5f45f7a3397e10d7e90558fd0d51b5437e5805
Mael RABOT <mael.rabot@epitech.eu>
2023-05-10 16:32:27 +0200

    Updage main page
    Signed-off-by: Mael RABOT <mael.rabot@epitech.eu>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+const getURL = "http://localhost:8080/get"
+
+function MyButton() {
+    return (
+        <button>Click to get user list</button>
+    )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <t> Hello World! </t>
+        <t>Hello World!</t>
+        <MyButton />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
