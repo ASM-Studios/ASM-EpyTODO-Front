8e87c9dfea644d14ec8b8a99c7ca9a35bdafc3e8
Mael RABOT <mael.rabot@epitech.eu>
2023-05-12 13:06:07 +0200

    Add jwt check
    Signed-off-by: Mael RABOT <mael.rabot@epitech.eu>

---
+++ src/utils/checks.js
@@ -0,0 +1,16 @@
+const Cookies = require("js-cookie")
+
+const isEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+const checkToken = (defaultURL = "http://localhost:3000") => {
+    const token = Cookies.get('token');
+
+    if (!token) {
+        window.location.href = defaultURL;
+    }
+}
+
+module.exports = { isEmail, checkToken }
